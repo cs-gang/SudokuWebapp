9b04e0a2439c3c48ab902e93bdf31ce830fdab38
Anand Krishna <anandkrishna2312@gmail.com>
2020-10-10 13:23:52 +0400

    Added index page to django mainapp

---
+++ sudoku-django-project/mainapp/urls.py
@@ -0,0 +1,7 @@
+#Defines paths to different views in the mainapp app
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- sudoku-django-project/mainapp/views.py
+++ sudoku-django-project/mainapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(response):
+    return render(response, "mainapp/index.html", {})
