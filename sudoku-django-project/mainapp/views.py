f46288d02dc11206c2605cde965d64f701f78810
Anand Krishna <anandkrishna2312@gmail.com>
2020-10-13 14:35:02 +0400

    Edited files to include type hints

--- sudoku-django-project/mainapp/views.py
+++ sudoku-django-project/mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django import http
 # Create your views here.
 
-def index(response):
+def index(response: http.HttpRequest) -> http.HttpResponse:
     return render(response, "mainapp/index.html", {})
