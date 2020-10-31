789b2ace8e0ceb0b2c48f830aebc02645aaaffc9
Anand Krishna <anandkrishna2312@gmail.com>
2020-10-31 12:38:57 +0400

    Made models

--- sudoku-django-project/mainapp/admin.py
+++ sudoku-django-project/mainapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Leaderboard
 # Register your models here.
+
+admin.site.register(Leaderboard)    # allows us to edit leaderboard from the admin site.
--- sudoku-django-project/mainapp/models.py
+++ sudoku-django-project/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class GameBoards(models.Model):
+    # An autoincrementing ID column which will be used as primary key is automatically added.
+    board = models.TextField(unique=True)
+
+    def __str__(self):
+        return self.board
+
+class Leaderboard(models.Model):
+    name = models.CharField(max_length=20, default="Player", blank=False)
+    time = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
