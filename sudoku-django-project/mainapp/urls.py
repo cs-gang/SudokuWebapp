c41a0d0836f3cd632e4fa813f27e1463cb255233
Anand Krishna <anandkrishna2312@gmail.com>
2020-10-31 15:52:19 +0400

    Did url config, made some helper db functions.

--- sudoku-django-project/mainapp/urls.py
+++ sudoku-django-project/mainapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
+    path("leaderboard", views.leaderboard, name="leaderboard")
 ]
--- sudoku-django-project/mainapp/views.py
+++ sudoku-django-project/mainapp/views.py
@@ -4,3 +4,26 @@ from django import http
 
 def index(response: http.HttpRequest) -> http.HttpResponse:
     return render(response, "mainapp/index.html", {})
+# this whitespace is me trying to avoid merge conflicts later, I'll remove it later ~ anand.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def leaderboard(request: http.HttpRequest) -> http.HttpResponse:
+    # do data retrieval
+    context = {}  # and put that data in context to be passed to the view.
+    return render(request, "mainapp/leaderboard.html", context)
