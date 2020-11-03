70071d80ebdb3835f76e1267a12ef54eae96d1d3
Anand Krishna <anandkrishna2312@gmail.com>
2020-11-03 19:56:44 +0400

    Handling POST request

--- sudoku-django-project/mainapp/views.py
+++ sudoku-django-project/mainapp/views.py
@@ -1,41 +1,63 @@
 from django.shortcuts import render
 from django import http
+from django.views.decorators.http import require_http_methods, require_GET
+from django.db.models import Max
 import json
 
 from utils.classes import BoardsQueue
 from utils.exceptions import QueueUnderflowError
-from .models import GameBoards
+from .models import GameBoards, Leaderboard
 
+# queue initialization 
 queue = BoardsQueue()
 
 lower, upper = 0, 11    # ID range to load to queue
-
+# fillinf queue with tables saved on the database
 for result in GameBoards.objects.filter(id__in=list(range(lower, upper))):
     queue.enqueue([result.id, json.loads(result.game_board), json.loads(result.check_board)])
 else:
     lower, upper = 11, 21
 
+
 # Create your views here.
+@require_GET
 def index(request: http.HttpRequest) -> http.HttpResponse:
     return render(request, "mainapp/index.html", {})
 
+@require_http_methods(['GET', 'POST'])
 def game(request: http.HttpRequest) -> http.HttpResponse:
-    global lower, upper
-    try:
-        board_id, game_board, check_board = queue.dequeue()
-    except QueueUnderflowError:
-        if upper == 101:        #there are currently only 100 items on the database, so it loops back to the starting.
-            lower, upper = 0, 11
-        else:
-            lower += 10
-            upper += 10
+    if request.method == 'GET':
+        try:
+            board_id, game_board, check_board = queue.dequeue()
+        except QueueUnderflowError:
+            if upper == 101:        #there are currently only 100 items on the database, so it loops back to the starting.
+                lower, upper = 0, 11
+            else:
+                lower += 10
+                upper += 10
 
-        for result in GameBoards.objects.filter(id__in=list(range(lower, upper))):  #loading new tables from database
-            queue.enqueue([result.id, json.loads(result.game_board), json.loads(result.check_board)])
-        else:
-            lower, upper = upper, upper + 10
+            for result in GameBoards.objects.filter(id__in=list(range(lower, upper))):  #loading new tables from database
+                queue.enqueue([result.id, json.loads(result.game_board), json.loads(result.check_board)])
+            else:
+                lower, upper = upper, upper + 10
 
-        board_id, game_board, check_board = queue.dequeue()
-    context = {'board_id': board_id, 'game_board': game_board, 'check_board': check_board}
-    
-    return render(request, "mainapp/game.html", context)
+            board_id, game_board, check_board = queue.dequeue()
+        context = {'board_id': board_id, 'game_board': game_board, 'check_board': check_board}
+        
+        return render(request, "mainapp/game.html", context)
+    else:
+        username = request.POST['username']
+        time = int(request.POST['time'])
+
+        current_worst = Leaderboard.objects.all().aggregate(Max('time'))
+
+        if time < current_worst['time__max']:
+            new = Leaderboard(name=username, time=time)
+            new.save()
+            on_leaderboard = True
+        else:
+            on_leaderboard = False
+
+        context = {'result': True, 'on_leaderboard': on_leaderboard}
+
+        return render(request, "mainaapp/game.html", context) 
