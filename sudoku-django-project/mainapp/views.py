be795272463b8d52c275eb4e7a9b8e032601f261
Anand Krishna <anandkrishna2312@gmail.com>
2020-11-03 19:24:56 +0400

    Function now also gives check_board

--- sudoku-django-project/mainapp/views.py
+++ sudoku-django-project/mainapp/views.py
@@ -11,7 +11,7 @@ queue = BoardsQueue()
 lower, upper = 0, 11    # ID range to load to queue
 
 for result in GameBoards.objects.filter(id__in=list(range(lower, upper))):
-    queue.enqueue([result.id, json.loads(result.game_board)])
+    queue.enqueue([result.id, json.loads(result.game_board), json.loads(result.check_board)])
 else:
     lower, upper = 11, 21
 
@@ -22,7 +22,7 @@ def index(request: http.HttpRequest) -> http.HttpResponse:
 def game(request: http.HttpRequest) -> http.HttpResponse:
     global lower, upper
     try:
-        board_id, game_board = queue.dequeue()
+        board_id, game_board, check_board = queue.dequeue()
     except QueueUnderflowError:
         if upper == 101:        #there are currently only 100 items on the database, so it loops back to the starting.
             lower, upper = 0, 11
@@ -31,11 +31,11 @@ def game(request: http.HttpRequest) -> http.HttpResponse:
             upper += 10
 
         for result in GameBoards.objects.filter(id__in=list(range(lower, upper))):  #loading new tables from database
-            queue.enqueue([result.id, result.game_board])
+            queue.enqueue([result.id, json.loads(result.game_board), json.loads(result.check_board)])
         else:
             lower, upper = upper, upper + 10
 
-        board_id, game_board = queue.dequeue()
-    context = {'board_id': board_id, 'game_board': game_board}
+        board_id, game_board, check_board = queue.dequeue()
+    context = {'board_id': board_id, 'game_board': game_board, 'check_board': check_board}
     
     return render(request, "mainapp/game.html", context)
