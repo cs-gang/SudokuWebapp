13a45e44046d673d67edb4431d336f56190470fa
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2020-11-04 09:26:54 +0400

    Fixed small syntax error
    Thank you flake8 :weary:

--- sudoku-django-project/mainapp/urls.py
+++ sudoku-django-project/mainapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
 
     path("game", views.game, name="game"),
-    path("result/<str:username>/<int:time>", views.result, name="result")
+    path("result/<str:username>/<int:time>", views.result, name="result"),
     path("leaderboard/<home>", views.leaderboard, name="leaderboard")
 ]
